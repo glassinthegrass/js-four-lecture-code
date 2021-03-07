f0e67da5e63f2f51f4fbc394fb3750c938dfa3f2
Jared Andersen <thejaredandersen@gmail.com>
2021-03-07 08:21:02 -0700

    js lecture retread

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const paragraph = document.getElementById('some-text')
 paragraph.innerText = 'I am new text given by JS'
 // console.log(paragraph.innerText)
 
+
 // console.log(paragraph.className)
 paragraph.className = 'some-class'
 // console.log(paragraph.className)
@@ -66,33 +67,49 @@ document.querySelector('form').addEventListener('submit', function(event){
 
 // * MINI-PROJECT CODE * //
 
-// Logic
-// User can type todo into the input
-// When user clicks add, todo is added to the list below the input
-// When user clicks a todo, it removes it from the list
+// logic
+//User can type todo into the input
+//when user clicks add, todo is added to the list below the input
+//when user clicks a todo,it removes it from the list
+//if you need to track user input, you first need to acess the user input, then once acessed you can put an event on the user input, then you can change a variable to track it, using event.target.value
+
 
 const todoInput = document.getElementById('todo-input')
 let todoInputValue = ''
 
-todoInput.addEventListener('change', function(event){
+todoInput.addEventListener('change', function (event) {
     todoInputValue = event.target.value
 })
 
-function createTodo(){
-    // 1. create a list item
-    // 2. give li text content from todoInputValue
-    // 3. add an event to li to allow the user to remove it
-    // 4. attach element to the ul
-
+const createTodo = () => {
+//1. create a list item
+//2. give li text content from todoInputValue
+//3. add event to li to allow removal
+//4. attach element to the ul
     const newTodo = document.createElement('li')
     newTodo.innerText = todoInputValue
-
-    newTodo.addEventListener('click', function(){
+    
+    newTodo.addEventListener('click', function () {
         newTodo.remove()
     })
 
     document.querySelector('#task-list').appendChild(newTodo)
-
     todoInput.value = ''
     todoInputValue = ''
+}
+
+
+//extra practice
+
+const count = document.getElementById('count')
+const decrementInput = document.getElementById('decrement')
+const clearInput = document.getElementById('clear')
+const incrementInput = document.getElementById('increment')
+
+const number = 0
+
+const decrement = () => {
+decrementInput.addEventListener('click', function () {
+    count.innerText(number-= 1)
+})
 }
